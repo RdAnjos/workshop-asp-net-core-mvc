1646f966569917be59c146f143349303f695b8a2
RdAnjos <rodrigo.oda@hotmail.com>
2023-06-01 20:58:57 -0300

    SallerFormViewModel and Department select component

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -16,7 +16,7 @@ namespace SalesWebMvc.Models
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
-
+         
         public Seller()
         {
         }
--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -45,6 +45,7 @@ namespace SalesWebMvc
             //Registrando na dependencia de injecao do sistema
             services.AddScoped<SeedingService>();
             services.AddScoped<SellerService>();
+            services.AddScoped<DepartmentService>(); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
